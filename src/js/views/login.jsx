91025019f018bb50612a60e8cb876b7c6461eeb7
elioaca <elio.colmenares@correo.unimet.edu.ve>
2025-03-15 02:22:37 -0400

    vite listo para traerse

--- src/js/views/login.jsx
+++ src/js/views/login.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
-// import {
-//     createUserWithEmailAndPassword, // Corregido aquí
-//     signInWithEmailAndPassword,
-//     GoogleAuthProvider,
-//     FacebookAuthProvider,
-//     signInWithPopup
-// } from "firebase/auth";
-// import { auth } from "../firebase.js";
+import {
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    signInWithPopup
+} from "firebase/auth";
+import { auth } from "../firebase.js";
 import "../../styles/login.css";
 
 const Login = () => {
